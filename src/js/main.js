6a68632a5a9f949a49dc31553526334dc15de073
Natasha Vicens <nkhan@publicsource.org>
2021-01-21 20:49:48 -0500

    text and photos in

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 // require("./lib/pym");
-require("./hamburger");
-require("./scrollama");
-require("./typeit");
-require("./tooltip");
-require("./expander");
+// require("./hamburger");
+// require("./scrollama");
+// require("./typeit");
+// require("./tooltip");
+// require("./expander");
